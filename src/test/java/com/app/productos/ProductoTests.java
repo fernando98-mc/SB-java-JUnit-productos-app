68fcc7921bdd278b8c30744a4b2bf7f075f8eed1
fernando98-mc <luisf543678@gmail.com>
2024-01-23 19:54:07 -0600

    add method testGuardarProducto() in package tests

--- src/test/java/com/app/productos/ProductoTests.java
+++ src/test/java/com/app/productos/ProductoTests.java
@@ -1,6 +1,11 @@
 package com.app.productos;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.app.productos.entity.Producto;
+import com.app.productos.repository.ProductoRepositorio;
 
 /*
  * Pruebas unitarias para las entidadas JPA
@@ -8,5 +13,14 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
  */
 @DataJpaTest
 public class ProductoTests {
+
+	@Autowired
+	private ProductoRepositorio productoRepositorio;
+
+	@Test
+	public void testGuardarProducto() {
+		Producto producto = new Producto("Malteada", 30);
+		productoRepositorio.save(producto);
+	}
 
 }
