45564b1c42de270789a41ce7d689e348e71c1322
fernando98-mc <luisf543678@gmail.com>
2024-01-23 19:48:09 -0600

    add @DataJpaTest in package tests

---
+++ src/test/java/com/app/productos/ProductoTests.java
@@ -0,0 +1,12 @@
+package com.app.productos;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+/*
+ * Pruebas unitarias para las entidadas JPA
+ * [inserciones a bases de datos]
+ */
+@DataJpaTest
+public class ProductoTests {
+
+}
